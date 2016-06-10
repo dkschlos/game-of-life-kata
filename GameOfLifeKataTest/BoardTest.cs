99f3b165239bbe16447bbd8a300736999b6a074f
Dominik Schlosser <dominik.schlosser@gmail.com>
2016-06-10 20:34:22 +0200

    Test example

---
+++ GameOfLifeKataTest/BoardTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeKataTest
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            int theAnswer = 42;
+
+            Assert.AreEqual(42, theAnswer);
+        }
+    }
+}
--- GameOfLifeKataTest/Properties/AssemblyInfo.cs
+++ GameOfLifeKataTest/Properties/AssemblyInfo.cs
@@ -2,25 +2,25 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Allgemeine Informationen über eine Assembly werden über folgende 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("GameOfLifeKata.Test")]
+[assembly: AssemblyTitle("GameOfLifeKataTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameOfLifeKata.Test")]
+[assembly: AssemblyProduct("GameOfLifeKataTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
 [assembly: ComVisible(false)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-[assembly: Guid("12ec98a9-c9f3-4e98-9f3e-c5b2096e370f")]
+[assembly: Guid("8d09bfbd-89c5-46ab-9b11-76ba5faaa0d0")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
+// [Assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
