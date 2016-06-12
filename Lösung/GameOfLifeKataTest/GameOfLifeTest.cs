25538f2cac8434e40fbdc8292532b7e9c2ec8a2b
Dominik Schlosser <dominik.schlosser@gmail.com>
2016-06-12 19:37:08 +0200

    Fix

--- Lösung/GameOfLifeKataTest/GameOfLifeTest.cs
+++ Lösung/GameOfLifeKataTest/GameOfLifeTest.cs
@@ -86,30 +86,7 @@ namespace GameOfLifeKataTest
             boardFactory.Setup(b => b.Create(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<NeighborshipInitializer>()))
                         .Returns(new Board(width, height, boardConfiguration));
 
-            MoqShowcase();
             return boardFactory.Object;
-        }
-
-
-        private interface Useless
-        {
-            int GetResult(long param);
-        }
-
-        private void MoqShowcase()
-        {
-            var usefulMock = new Mock<Useless>();
-            // Calls to this mock will always return 42
-            usefulMock.Setup(u => u.GetResult(It.IsAny<long>())).Returns(42);
-
-            Useless instanceOfMockedInterface = usefulMock.Object;
-            // Pass as dependency, for example:
-            // var service = new Service(instanceofMockedInterface);
-            // service.Run();
-
-            // Verify method was called with any parameter or explicit param value (line 2)
-            usefulMock.Verify(u => u.GetResult(It.IsAny<long>()));
-            usefulMock.Verify(u => u.GetResult(21));
         }
     }
 }
